932cb65097321079cc9d2da3ef1cea8d8c41269c
pavloveone <pavloveone@mail.ru>
2024-03-13 01:20:20 +0300

    fix cards

--- task3/src/app/page.tsx
+++ task3/src/app/page.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
-import Blog from "@/pages/blog";
 
 export default function Home() {
-  return <Link href="/blog">Перейти в каталог</Link>;
+  return (
+    <Link
+      className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      href="/games"
+    >
+      Перейти в каталог
+    </Link>
+  );
 }
